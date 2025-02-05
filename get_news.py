c0194d25daad161eca0ccc402c984322bc06233a
Torimaruboy <038.mina@gmail.com>
2025-02-05 21:27:31 +0900

    v2025.2.5

--- get_news.py
+++ get_news.py
@@ -1,11 +1,11 @@
 import os
+import requests
 import google.generativeai as genai
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from time import sleep
 
 os.system("taskkill /F /IM chrome.exe")
 options = Options()
@@ -18,6 +18,7 @@ driver.implicitly_wait(10)
 genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 model = genai.GenerativeModel("gemini-1.5-flash")
 
+# トップニュースのURLとタイトルを取得
 driver.get("https://news.yahoo.co.jp")
 news = (
     driver.find_element(By.ID, "uamods-topics")
@@ -26,19 +27,26 @@ news = (
 )
 urls = [(e.get_attribute("href")) for e in news]
 titles = [(e.text) for e in news]
+
+# 記事の掲載日時、タイトル、記事要約文を格納
 news_contents = []
-print("実行中", end="")
-for i in range(8):
-    driver.get(urls[i])
+for url, title in zip(urls, titles):
+    driver.get(url)
     driver.find_element(By.LINK_TEXT, "記事全文を読む").click()
     time = driver.find_element(By.TAG_NAME, "time").text
     article = driver.find_element(By.CLASS_NAME, "article_body").text
+    image_url = (
+        driver.find_element(By.CLASS_NAME, "article_body")
+        .find_element(By.TAG_NAME, "img")
+        .get_attribute("src")
+    )
+    # 画像を保存する
+    # with open(os.path.join("images", "image.jpg"), "wb") as f:
+    #     f.write(requests.get(image_url).content)
     prompt = model.generate_content(
         "以下はとあるニュース記事の全文です。この文章を100文字以内で要約してください:"
         + article
-    )
-    news_contents.append((time, titles[i], prompt.text))
-    print(".", end="")
+    ).text
+    news_contents.append((time, title, prompt))
 
-print("\n実行完了")
 pass
